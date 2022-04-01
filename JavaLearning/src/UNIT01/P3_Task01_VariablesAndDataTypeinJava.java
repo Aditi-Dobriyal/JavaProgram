0c1717ba19b6f554ee3f65a1542f8410fed057df
Aditi <aditidobriyal111@gmail.com>
2022-04-01 20:25:56 +0530

    lab3 commit

---
+++ JavaLearning/src/UNIT01/P3_Task01_VariablesAndDataTypeinJava.java
@@ -0,0 +1,18 @@
+package UNIT01;
+
+/*
+-Variable in java 
+ */
+
+public class P3_Task01_VariablesAndDataTypeinJava {
+
+	public P3_Task01_VariablesAndDataTypeinJava() {
+		
+	}
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
