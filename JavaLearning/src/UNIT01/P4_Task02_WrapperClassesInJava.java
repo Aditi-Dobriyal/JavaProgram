40c1eb5a28bf7cb12023175cff9a915302bcc116
Aditi <aditidobriyal111@gmail.com>
2022-04-10 20:23:07 +0530

    Commit P4,P5,P6,P7,P8,P9

---
+++ JavaLearning/src/UNIT01/P4_Task02_WrapperClassesInJava.java
@@ -0,0 +1,41 @@
+package UNIT01;
+
+/*
+ * The wrapper class in Java provides the mechanism to convert primitive into object and
+ * Why?
+ * Because we want to use predefined methods of those objects
+ * - boolean data types -> Boolean obj; -> obj.toString();
+ *  
+ */
+
+public class P4_Task02_WrapperClassesInJava {
+
+
+	public static void main(String[] args) {
+		// Converting int into Integer
+		int a = 20;
+		
+		String s ="" + a + "";
+		
+		Integer i = Integer.valueOf(a); // converting int into Integer explicitly
+		Integer j = a;                  // autoboxing, now complier will write Interger.value0f(a) internally
+		
+		System.out.println(a + " " + " " + j);
+		
+		System.out.println(i.toString());
+		
+		// a.toString() can not be done to a is int not object
+		
+		//Autoboxing: converting primitives into objects
+		byte b=10;
+		Byte byteobj=b;
+		
+		System.out.println(byteobj);
+		
+		//Unboxing: Converting Objects to Primitives
+		byte bytevalue=byteobj;
+		System.out.println(bytevalue);
+
+	}
+
+}
